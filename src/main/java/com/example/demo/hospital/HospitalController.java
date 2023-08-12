b7c82bcdb1afa7f51e038b4ae5f93e033516552b
dvvteja <dvvteja@gmail.com>
2023-08-12 22:52:43 +0530

    Added SecurityConfig for Basic Auth

--- src/main/java/com/example/demo/hospital/HospitalController.java
+++ src/main/java/com/example/demo/hospital/HospitalController.java
@@ -1,8 +1,10 @@
 package com.example.demo.hospital;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,20 +16,46 @@ public class HospitalController {
     public List<Hospital> getAllHospitals(){
         return hs.getAllHospitals();
     }
+    List<Student> students= new ArrayList<>();
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/students")
+    public List<Student> students(){
+
+        Student student1=new Student();
+        student1.setId(1);
+        student1.setFirstName("Teja");
+        student1.setLastName("Dvv");
+        Student student2=new Student();
+        student2.setId(2);
+        student2.setFirstName("Teja");
+        student2.setLastName("Dvv");
+        students.add(student1);
+        students.add(student2);
+        return students;
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/students")
+    public Student createStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
     @GetMapping("/hospitals/{id}")
     public Hospital getHospitalByID(@PathVariable("id") int id){
         return hs.getHospitalByID(id);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/hospitals")
     public int saveHospitals(@RequestBody Hospital hospital){
         hs.saveHospitals(hospital);
         return hospital.getId();
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/hospitals")
     public int updateHospital(@RequestBody Hospital hospital){
         hs.updateHospital(hospital);
         return hospital.getId();
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/hospitals/{id}")
     public void deleteHospital(@PathVariable("id") int id){
         hs.deleteHospital(id);
---
+++ src/main/java/com/example/demo/hospital/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.hospital;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("teja")
+                .password(passwordEncoder().encode("pwd"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("adminpwd"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+}
